a3b9721ea01f70e4e0ddf7ce1b8cc49739851439
libpoet1312 <npappas@uth.gr>
2022-09-29 03:25:25 +0300

    pytest.ini

--- app/api.py
+++ app/api.py
@@ -4,16 +4,12 @@ import os
 import logging
 from flask_caching import Cache
 from config import BaseConfig
-from flask import Flask, jsonify
+from flask import Flask
 from flask_restx import Api, Resource, reqparse
 
 from consumer.consumer import Consumer
 
 logging.basicConfig(level=logging.INFO)
-
-# add this dir to Path so not to use relative imports
-API_DIR = os.path.dirname(os.path.abspath(__file__))
-sys.path.append(os.path.dirname(API_DIR))
 
 app = Flask(__name__)
 
@@ -21,21 +17,6 @@ app.config.from_object(BaseConfig)
 cache = Cache(app)
 
 api = Api(app)
-
-
-def make_response(func):
-    @functools.wraps(func)
-    def wrapper(*args, **kwargs):
-        print('wrapper', flush=True)
-        result = func(*args, **kwargs)
-        response = {
-            'success': True,
-            'result': result
-        }
-        status_code = 200
-        return jsonify(response), status_code
-    print('here')
-    return wrapper
 
 
 @cache.cached(timeout=30, query_string=True)
--- tests/test_api.py
+++ tests/test_api.py
@@ -1,37 +1,11 @@
-import pathlib
 import pytest
-import connexion
-from connexion.exceptions import InvalidSpecification
-from connexion.spec import canonical_base_path
-
-APP_FOLDER = pathlib.Path(__file__).parent.parent
+from app.api import app
 
 
-def test_invalid_schema_file_structure():
-    with pytest.raises(InvalidSpecification):
-        connexion.FlaskApi(
-            APP_FOLDER / "tests" / "INVALID_openapi3.0.yaml",
-            base_path="/api/v1",
-            debug=True,
-        )
-
-
-def test_canonical_base_path():
-    assert canonical_base_path("") == ""
-    assert canonical_base_path("/") == ""
-    assert canonical_base_path("/api") == "/api"
-    assert canonical_base_path("/api/") == "/api"
-
-
-def test_api():
-    app = connexion.FlaskApp(__name__, specification_dir='')
-    api = app.add_api(
-        APP_FOLDER / 'nCode_openapi3.0.yaml',
-        base_path='/api/v1',
-        options={"swagger_ui": True, "serve_spec": True},
-        validate_responses=True
-    )
-    assert api.blueprint.name == "/api/v1"
+def test_api(client):
+    response = client.get("/")
+    status_code = response.status_code
+    assert status_code == 200
 
 
 def test_api_404(client):
@@ -43,23 +17,7 @@ def test_statistics_endpoint_wrong_date_format(client):
     response = client.get('/api/v1/stackstats?since=2020/10/02 10:00:00')
     assert 400 == response.status_code
 
-
-@pytest.fixture()
-def connection_app():
-    app = connexion.FlaskApp(__name__, specification_dir='')
-    app.add_api(
-        APP_FOLDER / 'nCode_openapi3.0.yaml',
-        base_path='/api/v1',
-        options={"swagger_ui": True, "serve_spec": True},
-        validate_responses=True
-    )
-    yield app
-
-
-@pytest.fixture()
-def client(connection_app):
-    return connection_app.app.test_client()
-
-
-
-
+@pytest.fixture
+def client():
+    with app.test_client() as client:
+        yield client
